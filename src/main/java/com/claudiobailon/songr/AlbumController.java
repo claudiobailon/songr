19059f592170978ab01cab48b04c3c21853903dc
claudiobailon <c.bailonschubert@gmail.com>
2020-09-30 08:55:01 -0700

    Attempted to link sql

--- src/main/java/com/claudiobailon/songr/AlbumController.java
+++ src/main/java/com/claudiobailon/songr/AlbumController.java
@@ -1,13 +1,25 @@
 package com.claudiobailon.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 
 @Controller
 public class AlbumController {
+    @Autowired
+    public AlbumRepository albumRepository;
+
+    @PostMapping("/albums")
+    public RedirectView addAlbum(String title, String artist, int songCount, int length, String imageURL){
+        Album newAlbum = new Album(title, artist, songCount, length, imageURL);
+        albumRepository.save(newAlbum);
+        return new RedirectView("/albums");
+    }
 
     @GetMapping("/albums")//get route
     public String showAlbums(Model m){
